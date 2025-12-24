56adacbe3a12ff081fb57821f8be958a5c186be0
souvik-git-create <mynamesouvik452@gmail.com>
2025-12-24 23:39:16 +0530

    24-12-25 updated

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import { Card } from "../components";
 
 export const MovieList = () => {
+  const[movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=7c2969f73a68e73f60cbc7b7da7ba1a7");
+      const data = await response.json();
+      setMovies(data.results)
+    }
+    fetchMovies();
+  }, [movies])
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap md:gap-5">
-          <Card />
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
     </main>
